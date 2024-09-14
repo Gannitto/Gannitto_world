2599ef45a2a16cfe393f965d5cd1c56597659227
Gannitto <danilaserezhin@gmail.com>
2024-09-14 17:48:48 +0200

    Добавлена кнопка выхода из аккаунта

--- script.js
+++ script.js
@@ -27,6 +27,38 @@ document.getElementById('registration-form').addEventListener('submit', async fu
 
 		const data = await response.json();
 		alert('Регистрация прошла успешно!');
+
+		try {
+			const response = await fetch('https://gannittoworld-production.up.railway.app/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					username,
+					password
+				})
+			});
+
+			if (!response.ok) {
+				throw new Error(`Ошибка: ${response.statusText}`)
+			}
+
+			const data = await response.json();
+
+			if (data.token) {
+				// Сохраняем токен в localStorage для дальнейших запросов
+				localStorage.setItem('token', data.token)
+				localStorage.setItem('username', username)
+				alert('Вход выполнен успешно!')
+				window.open("index.html")
+			} else {
+				alert('Ошибка входа. Неверные данные.')
+			}
+		} catch (error) {
+			console.error('Ошибка при входе:', error)
+			alert(`Ошибка: ${error.message}`)
+		}
 		console.log('Ответ сервера:', data);
 		messageDiv.textContent = "Регистрация прошла успешно!"
 
